4bfaaf1520539902eac458f64cef4a3f328aef6f
Michael Mammoliti <mammoliti.michael@gmail.com>
2017-12-21 14:50:22 +0000

    Update viewport.js

--- viewport.js
+++ viewport.js
@@ -1,28 +1,85 @@
-class Viewport {
+class ViewportManager {
   constructor(options) {
-    this.sizes = options.sizes;
+    this.options = { ...options };
 
-    this.breakpoints = this.getBreakpoints();
+    this.initialise();
   }
 
-  getMediaQueryString(size) {
-    return `(min-width: ${size.min || 0}px) and (max-width: ${size.max || 10000}px)`;
+  // Initialise
+  // =============================================
+  initialise() {
+    const { delay, breakpoints } = this.options;
+
+    if (!breakpoints) {
+      console.warn(`It seems you didn't set any breakpoint. Check "options.breakpoints".`);
+
+      return;
+    }
+
+    this.breakpoints = this.getBreakpoints(breakpoints);
+    this.breakpointKeys = Object.keys(this.breakpoints);
+    this.listenerCollection = [];
+
+    window.addEventListener('resize', () => {
+      if (!delay) return this.handleWindowResize();
+
+      window.clearTimeout(this.timeout);
+      this.timeout = window.setTimeout(() => this.handleWindowResize(), delay);
+    });
   }
 
-  getBreakpoints() {
-    return Object.keys(this.sizes).reduce((acc, size) => ({
-      ...acc,
-      [size]: this.getMediaQueryString(sizes[size])
-    }), {});
+  // Events
+  // =============================================
+  handleWindowResize() {
+    const currentViewport = this.getViewport();
+
+    // if we don't have listeners, we return.
+    if (!this.listenerCollection.length) return;
+
+    // if same viewport we don't want to fire the functions
+    if (this.activeViewport === currentViewport) return;
+
+    // we set the viewport
+    this.activeViewport = currentViewport;
+
+    // we call all listeners.
+    this.listenerCollection.forEach(listenerFn => listenerFn(this.activeViewport));
   }
 
-  get() {
-    return Object.keys(this.breakpoints).reduce((acc, breakpointLimit) => {
+  // Getters
+  // =============================================
+  getViewport() {
+    return this.breakpointKeys.reduce((acc, breakpointLimit) => {
       if (window.matchMedia(this.breakpoints[breakpointLimit]).matches) return breakpointLimit;
 
       return acc;
     }, '');
   }
+
+  getMediaQueryString({ min = 0, max = 10000 }) {
+    return `(min-width: ${min}px) and (max-width: ${max}px)`;
+  }
+
+  getBreakpoints(breakpoints) {
+    return Object.keys(breakpoints).reduce((acc, size) => ({
+      ...acc,
+      [size]: this.getMediaQueryString(breakpoints[size]),
+    }), {});
+  }
+
+  // Listeners
+  // =============================================
+  listen(listenerFn) {
+    this.listenerCollection.push(listenerFn);
+  }
+
+  unlisten(listenerFn) {
+    const index = this.listenerCollection.indexOf(listenerFn);
+
+    if (index === -1) return;
+
+    this.listenerCollection.splice(index, 0);
+  }
 }
 
-export default Viewport;
+export default ViewportManager;
